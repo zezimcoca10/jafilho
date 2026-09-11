3a00bd3cf10208eb99bed7dd42b2339eaa386c37
zezimcoca10 <58553414+zezimcoca10@users.noreply.github.com>
2026-09-11 08:13:49 -0300

    fix: remove prettier from lint gate

--- eslint.config.js
+++ eslint.config.js
@@ -1,5 +1,4 @@
 import js from "@eslint/js";
-import eslintPluginPrettier from "eslint-plugin-prettier/recommended";
 import globals from "globals";
 import reactHooks from "eslint-plugin-react-hooks";
 import reactRefresh from "eslint-plugin-react-refresh";
@@ -34,8 +33,6 @@ export default tseslint.config(
       ],
       "react-refresh/only-export-components": ["warn", { allowConstantExport: true }],
       "@typescript-eslint/no-unused-vars": "off",
-      "prettier/prettier": "off",
     },
   },
-  eslintPluginPrettier,
 );
